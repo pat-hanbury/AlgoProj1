1e8e4c5f55b61a5a1eb15b51e426f1de562f618b
jmkang240 <35613765+jmkang240@users.noreply.github.com>
2018-01-20 17:32:21 -0500

    class response draft

---
+++ response.h
@@ -0,0 +1,22 @@
+//John Kang and Patrick Hanbury
+//Fundamental of Algorithms
+//Project1
+//header response.h
+
+#ifndef RESPONSE_H_INCLUDED
+#define RESPONSE_H_INCLUDED
+
+class response{
+private:
+
+public:
+    int Correct; //Variable that represents the number of correct digits
+    int Incorrect; //Variable that represents the number of correct digits in the wrong position.
+    void setResponse(code& winningCode, const code& guess); //Function that takes winningCode and guess as objects in order to set the variables Correct and Incorrect.
+    bool compareResponse(response& newResponse) //Function that takes the response object by reference and compares it to the response.
+    void printResponse(); //Function that prints the variables Correct and Incorrect
+    response(code& winningCode, const code& guess); //Constructor that takes winningCode and guess as objects by reference
+};
+
+
+#endif // RESPONSE_H_INCLUDED
