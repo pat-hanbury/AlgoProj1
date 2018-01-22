1d6112c1723dc1a6d8015a2622a002180baebe6c
[pat-hanbury] <[patrickhanbury1@gmail.com]>
2018-01-22 11:08:13 -0500

    made CmakeLists for Clion and updated includes in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include "mastermind.h"
 #include "mastermind.cpp"
+#include "code.h"
+
+//still need to write code.cpp
+//#include "code.cpp"
+
+#include "response.h"
+#include "response.cpp"
+
+using namespace std;
 
 int main() {
     mastermind gameObject; //intialize the object with all the power
